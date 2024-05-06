73a3b0725a0ac1893b696d902b25b5452c2c1808
jlancon8 <114144763+jlancon8@users.noreply.github.com>
2024-05-06 09:30:45 +0200

    Update RosesPawn.java

--- src/model/RosesPawn.java
+++ src/model/RosesPawn.java
@@ -15,6 +15,8 @@ public class RosesPawn extends GameElement {
     public static int PAWN_BLUE = 0;
     public static int PAWN_RED = 1;
 
+    public static int PAWN_YELLOW = 2;
+
     public RosesPawn(int number, int color, GameStageModel gameStageModel) {
         super(gameStageModel);
         // registering element types defined especially for this game
@@ -24,12 +26,24 @@ public class RosesPawn extends GameElement {
         this.color = color;
     }
 
+    public RosesPawn(int color, GameStageModel gameStageModel) {
+        super(gameStageModel);
+        // registering element types defined especially for this game
+        ElementTypes.register("pawn",50);
+        type = ElementTypes.getType("pawn");
+        this.color = color;
+    }
+
     public int getNumber() {
         return number;
     }
 
     public int getColor() {
         return color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 
 }
