190fa4fe6a057c6ffa88c28c40ed43f9f0365c26
jlancon8 <114144763+jlancon8@users.noreply.github.com>
2024-05-22 12:57:50 +0200

    la fin de neuille land
    gt

--- src/control/RosesDecider.java
+++ src/control/RosesDecider.java
@@ -4,14 +4,16 @@ import boardifier.control.ActionFactory;
 import boardifier.control.Controller;
 import boardifier.control.Decider;
 import boardifier.model.GameElement;
+import boardifier.model.GameStageModel;
 import boardifier.model.Model;
 import boardifier.model.action.ActionList;
-import model.RosesBoard;
-import model.RosesPawnPot;
-import model.RosesStageModel;
-import model.RosesPawn;
+import model.*;
+import view.RosesStageView;
 
+import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
@@ -20,47 +22,170 @@ public class RosesDecider extends Decider {
 
     private static final Random loto = new Random(Calendar.getInstance().getTimeInMillis());
 
+    private static final String[] typeOfMoves = {"M", "H", "P"};
+    private String[] possibleMovements = new String[5];
+    int[] possibleValues = new int[5];
+
+
     public RosesDecider(Model model, Controller control) {
         super(model, control);
     }
 
     @Override
     public ActionList decide() {
-        // do a cast get a variable of the real type to get access to the attributes of RosesStageModel
-        RosesStageModel stage = (RosesStageModel)model.getGameStage();
-        RosesBoard board = stage.getBoard(); // get the board
-        RosesPawnPot pot = null; // the pot where to take a pawn
-        GameElement pawn = null; // the pawn that is moved
-        int rowDest = 0; // the dest. row in board
-        int colDest = 0; // the dest. col in board
-
+        RosesStageModel stage = (RosesStageModel) model.getGameStage();
+        RosesBoard board = stage.getBoard();
+        RosesPawnPot pawnPot = null;
+        GameElement pawn = null;
+        GameElement yellowPawn = stage.getCrownPawn();
+        int rowDest = 0;
+        int colDest = 0;
+        int row = 0;
+        int col = 0;
+        int choice = 0;
+        ActionList actions = new ActionList();
+        int[][] redPositions = new int[26][2];
+        int redIndex = 0;
+        int nbMovements = 0;
+        RosesPawn tempPawn = null;
         if (model.getIdPlayer() == RosesPawn.PAWN_BLUE) {
-            pot = stage.getBluePot();
-        }
-        else {
-            pot = stage.getRedPot();
+            pawnPot = stage.getBluePot();
+        } else {
+            pawnPot = stage.getRedPot();
         }
 
-        for(int i=0;i<1;i++) {
-            RosesPawn p = (RosesPawn)pot.getElement(0,0);
-            // if there is a pawn in i.
-            if (p != null) {
-                // get the valid cells
-                List<Point> valid = board.computeValidCells("", 0);
-                if (!valid.isEmpty()) {
-                    // choose at random one of the valid cells
-                    int id = loto.nextInt(valid.size());
-                    pawn = p;
-                    rowDest = valid.get(id).y;
-                    colDest = valid.get(id).x;
-                    break; // stop the loop
+        if (model.getIdPlayer() == RosesPawn.PAWN_RED) {
+            for (int k = 0; k < stage.getPlayer2MovementCards().length; k++) {
+                if (stage.getPlayer2MovementCards()[k] != null) {
+                    possibleMovements[k] = stage.getPlayer2MovementCards()[k].getDirection();
+                    possibleValues[k] = stage.getPlayer2MovementCards()[k].getValue();
                 }
             }
+            for (int i = 0; i < board.getNbRows(); i++) {
+                for (int j = 0; j < board.getNbCols(); j++) {
+                    if (board.getElement(i, j) != null) {
+                        tempPawn= (RosesPawn)board.getElement(i,j);
+                        if (tempPawn.getColor() == RosesPawn.PAWN_RED) {
+                            redPositions[redIndex][0] = i;
+                            redPositions[redIndex][1] = j;
+                            redIndex++;
+                        } else if (tempPawn.getColor() == RosesPawn.PAWN_YELLOW) {
+                            row = i;
+                            col = j;
+                            System.out.println("neuille");
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < possibleMovements.length; i++) {
+                if (possibleMovements[i] == null) continue;
+                rowDest = row;
+                colDest = col;
+                switch (possibleMovements[i]) {
+                    case "W":
+                        colDest = col - possibleValues[i];
+                        choice = i;
+                        break;
+                    case "N-E":
+                        colDest = col + possibleValues[i];
+                        rowDest = row - possibleValues[i];
+                        choice = i;
+                        break;
+                    case "E":
+                        colDest = col + possibleValues[i];
+                        choice = i;
+                        break;
+                    case "S-E":
+                        colDest = col + possibleValues[i];
+                        rowDest = row + possibleValues[i];
+                        choice = i;
+                        break;
+                    case "S":
+                        rowDest = row + possibleValues[i];
+                        choice = i;
+                        break;
+                    case "S-W":
+                        rowDest = row + possibleValues[i];
+                        colDest = col - possibleValues[i];
+                        choice = i;
+                        break;
+                    case "N":
+                        rowDest = row - possibleValues[i];
+                        choice = i;
+                        break;
+                    default:
+                        rowDest = row - possibleValues[i];
+                        colDest = col - possibleValues[i];
+                        choice = i;
+                }
+                System.out.println("choice : " + choice);
+                System.out.println("Checking destination: rowDest =" + rowDest + " colDest =" + colDest);
+                System.out.println("Row : " + row + ", col : " + col);
+                if (board.canReachCell(rowDest, colDest)) {
+                    for (int m = 0; m < redIndex; m++) {
+                        if (rowDest - 1 == redPositions[m][0] && (!board.isElementAt(rowDest, colDest))) {
+                            actions = doAction(nbMovements, model, pawn, "RoseBoard", rowDest, colDest, choice);
+                            return actions;
+                        } else if (rowDest + 1 == redPositions[m][0] && (!board.isElementAt(rowDest, colDest))) {
+                            actions = doAction(nbMovements, model, pawn, "RoseBoard", rowDest, colDest, choice);
+                            return actions;
+                        }
+                        if (redPositions[m][1] >= 0) {
+                            if (colDest + 1 == redPositions[m][1] && (!board.isElementAt(rowDest, colDest))) {
+                                actions = doAction(nbMovements, model, pawn, "RoseBoard", rowDest, colDest, choice);
+                                return actions;
+                            } else if (rowDest + 1 == redPositions[m][1] && (!board.isElementAt(rowDest, colDest))) {
+                                actions = doAction(nbMovements, model, pawn, "RoseBoard", rowDest, colDest, choice);
+                                return actions;
+                            }
+                        }
+                    }
+                    if (!board.isElementAt(rowDest, colDest)) {
+                        actions = doAction(nbMovements, model, pawn, "RoseBoard", rowDest, colDest, choice);
+                        return actions;
+                    }
+                }
+
+            }
+                for (int i = 0; i < stage.getPlayer2MovementCards().length; i++) {
+                    if (stage.getPlayer2MovementCards()[i] == null && stage.getPickCards().length > 0) {
+                        stage.getPlayer2MovementCards()[i] = stage.getPickCards()[stage.getPickCards().length - 1];
+                        stage.getPlayer2MovementCards()[i].flip();
+                        stage.getMoovRedPot().addElement(stage.getPlayer2MovementCards()[i], i, 0);
+                        setTempPickCards(stage);
+
+                    }
+                }
         }
+        return null;
+    }
 
-        ActionList actions = ActionFactory.generatePutInContainer( model, pawn, "Rosesboard", rowDest, colDest);
-        actions.setDoEndOfTurn(true); // after playing this action list, it will be the end of turn for current player.
 
+
+    private static ActionList doAction(int nbMovements, Model model, GameElement pawn, String name, int rowDest, int colDest, int choice) {
+        RosesStageModel stage = (RosesStageModel) model.getGameStage();
+        pawn = stage.getRedPot().getElement(0, 0, nbMovements);
+        ActionList actions = ActionFactory.generatePutInContainer(model, pawn, name, rowDest, colDest);
+        stage.removeElement(stage.getPlayer2MovementCards()[choice]);
+        stage.getDiscardCards()[nbMovements] = stage.getPlayer2MovementCards()[choice];
+        stage.getPlayer2MovementCards()[choice].flip();
+        stage.getDiscardPot().addElement(stage.getPlayer2MovementCards()[choice], 0, 0);
+        stage.getPlayer2MovementCards()[choice] = null;
+        stage.getBoard().moveElement(stage.getCrownPawn(), rowDest, colDest);
+        nbMovements++;
+        actions.setDoEndOfTurn(true);
         return actions;
     }
+
+    public void setTempPickCards(RosesStageModel stageModel) {
+        RosesCard[] tempPickCard = stageModel.getPickCards();
+        RosesCard[] copyOfPickPotCards = new RosesCard[tempPickCard.length - 1];
+        System.arraycopy(tempPickCard, 0, copyOfPickPotCards, 0, copyOfPickPotCards.length);
+        tempPickCard = copyOfPickPotCards;
+        stageModel.setPickCards(tempPickCard);
+    }
+
+
+
 }
