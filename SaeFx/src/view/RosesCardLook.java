abd07f7ac488d246fea02f6b74192ffbfa2b606f
ValentinV-J <144365848+ValentinV-J@users.noreply.github.com>
2024-06-03 14:56:47 +0200

    sae fx

---
+++ SaeFx/src/view/RosesCardLook.java
@@ -0,0 +1,68 @@
+//package view;
+//
+//import boardifier.model.GameElement;
+//import boardifier.view.ConsoleColor;
+//import boardifier.view.ElementLook;
+//import model.RosesCard;
+//
+//import java.util.Objects;
+//
+//import static model.RosesCard.CARD_BLUE;
+//import static model.RosesCard.CARD_RED;
+//
+//public class RosesCardLook extends ElementLook {
+//
+//    public RosesCardLook(GameElement element) {
+//        super(element, 1);
+//        setAnchorType(ANCHOR_TOPLEFT);
+//    }
+//
+//    protected void render() {
+//        RosesCard card = (RosesCard) element;
+//        if (Objects.equals(card.getCardType(), "MOUVEMENT")) {
+//            this.setSize(1, 2);
+//            switch (card.getDirection()) {
+//                case "N":
+//                    shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + "↑" + ConsoleColor.RESET;
+//                    break;
+//                case "N-E":
+//                    shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + "↗" + ConsoleColor.RESET;
+//                    break;
+//                case "E":
+//                    shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + "→" + ConsoleColor.RESET;
+//                    break;
+//                case "S-E":
+//                    shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + "↘" + ConsoleColor.RESET;
+//                    break;
+//                case "S":
+//                    shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + "↓" + ConsoleColor.RESET;
+//                    break;
+//                case "S-W":
+//                    shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + "↙" + ConsoleColor.RESET;
+//                    break;
+//                case "W":
+//                    shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + "←" + ConsoleColor.RESET;
+//                    break;
+//                default:
+//                    shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + "↖" + ConsoleColor.RESET;
+//                    break;
+//            }
+//            shape[1][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + card.getValue() + ConsoleColor.RESET;
+//            if (card.isFlipped()) {
+//                shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + " " + ConsoleColor.RESET;
+//                shape[1][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + " " + ConsoleColor.RESET;
+//            }
+//        }else{
+//            if (card.getColor() == CARD_BLUE) {
+//                shape[0][0] = ConsoleColor.BLACK + ConsoleColor.BLUE_BACKGROUND + "H" + ConsoleColor.RESET;
+//            } else if (card.getColor() == CARD_RED){
+//                shape[0][0] = ConsoleColor.BLACK + ConsoleColor.RED_BACKGROUND + "H" + ConsoleColor.RESET;
+//            }
+//            if (card.isFlipped()) {
+//                shape[0][0] = ConsoleColor.BLACK + ConsoleColor.WHITE_BACKGROUND + " " + ConsoleColor.RESET;
+//            }
+//        }
+//
+//    }
+//}
+//
---
+++ SaeFx/src/view/RosesStageView.java
@@ -0,0 +1,48 @@
+package view;
+
+import boardifier.model.GameStageModel;
+import boardifier.view.*;
+import javafx.stage.Screen;
+import model.RosesStageModel;
+
+public class RosesStageView extends GameStageView {
+    public RosesStageView(String name, GameStageModel gameStageModel) {
+        super(name, gameStageModel);
+        width = (int) Screen.getPrimary().getBounds().getWidth(); // get the resolution of the user and set it to the stage
+        height = (int) Screen.getPrimary().getBounds().getHeight();
+
+
+    }
+
+    @Override
+    public void createLooks() {
+        RosesStageModel model = (RosesStageModel)gameStageModel;
+
+        addLook(new RosesBoardLook(150, model.getBoard()));
+        addLook(new BluePawnPotLook(model.getBluePot()));
+        addLook(new RedPawnPotLook(model.getRedPot()));
+
+        for(int i=0;i<26;i++) {
+            addLook(new PawnLook(17,model.getBluePawns()[i]));
+            addLook(new PawnLook(17, model.getRedPawns()[i]));
+        }
+
+        addLook(new TextLook(24, "0x000000", model.getPlayerName()));
+        addLook(new RosesCardPotLook(70, 55, model.getMoovBluePot()));
+        addLook(new RosesCardPotLook(70, 55, model.getMoovRedPot()));
+        addLook(new RosesCardPotLook(80, 65, model.getBlueHeroPot()));
+        addLook(new RosesCardPotLook(80, 65, model.getRedHeroPot()));
+        addLook(new RosesCardPotLook(80, 40, model.getDiscardPot()));
+        addLook(new RosesCardPotLook(80, 40, model.getPickPot()));
+
+        /* Example to show how to set a global container to layout all looks in the root pane
+           Must also uncomment lines in HoleStageFactory and HoleStageModel
+        ContainerLook mainLook = new ContainerLook(model.getRootContainer(), -1);
+        mainLook.setPadding(10);
+        mainLook.setVerticalAlignment(ContainerLook.ALIGN_MIDDLE);
+        mainLook.setHorizontalAlignment(ContainerLook.ALIGN_CENTER);
+        addLook(mainLook);
+
+         */
+    }
+}
