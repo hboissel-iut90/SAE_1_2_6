e6fc2833c8b8d827bb0c406dfd1dee51a6fb6f72
user1 <harry.boisselot@edu.univ-fcomte.fr>
2024-06-15 12:51:47 +0200

    Modification des commentaires pour réadapter par rapport à la classe

--- src/model/RosesCardPot.java
+++ src/model/RosesCardPot.java
@@ -8,13 +8,13 @@ public class RosesCardPot extends ContainerElement {
     public int height;
 
     public RosesCardPot(String namePot, int x, int y, GameStageModel gameStageModel) {
-        // call the super-constructor to create a 4x1 grid, named "cardpot", and in x,y in space
+        // call the super-constructor to create a 4x1 grid, named by namePot, and in x,y in space
         super(namePot, x, y, 1, 5, gameStageModel);
     }
 
     public RosesCardPot(int x, int y, int width, int height, GameStageModel gameStageModel) {
         // call the super-constructor to create a grid with specified width and height,
-        // named "cardpot", and at position x,y in space
+        // named "othercardpot", and at position x,y in space
         super("othercardpot", x, y, width, height, gameStageModel);
         this.width = width;
         this.height = height;
@@ -23,13 +23,13 @@ public class RosesCardPot extends ContainerElement {
 
     public RosesCardPot(String namePot, int x, int y, int width, int height, GameStageModel gameStageModel) {
         // call the super-constructor to create a grid with specified width and height,
-        // named "cardpot", and at position x,y in space
+        // named by namePot, and at position x,y in space
         super(namePot, x, y, width, height, gameStageModel);
         this.width = width;
         this.height = height;
     }
-    /*
 
+    /*
     public RosesCardPot(int x, int y, int width, int height, GameStageModel gameStageModel, boolean isDiscardPot) {
         // call the super-constructor to create a grid with specified width and height,
         // named "cardpot", and at position x,y in space
@@ -45,8 +45,7 @@ public class RosesCardPot extends ContainerElement {
         this.width = width;
         this.height = height;
     }
-
-     */
+    */
 
     public int getWidth() {
         return width;
